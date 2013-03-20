97a0ae2225a027d6ddf6ebd11653b10b27490a76
Devon Noonan <dnoonan@infotech.com>
2013-03-20 10:27:08 -0400

    Removed livequery and tablesorter. Added dependency for datatables. Converted to datatables, removed unneccessary styling, js.

--- app/assets/javascripts/mails_viewer.js
+++ app/assets/javascripts/mails_viewer.js
@@ -1,25 +1,28 @@
 //= require jquery
+//= require dataTables/jquery.dataTables
 //= require_tree .
 
 $(function() {
-  $.livequery.registerPlugin("tablesorter", "tablesorterPager");
+  $("table").dataTable({
+    "aaSorting": [[3, "desc"]],
+    "aoColumns": [
+      null,
+      null,
+      null,
+      null,
+      { "bSortable": false }
+    ]
+  });
 
-  $('.mail a.preview').livequery(function() {
+  $('.mail a.preview').on('click', function() {
     console.log(this);
     $(this).colorbox({width:"80%", height:"80%", iframe:true});
   });
 
-  $('.mail a.raw').live('click', function(){
+  $('.mail a.raw').on('click', function(){
     $.get($(this).attr('href'), function(data){
       $('#raw div').text(data);
     });
     return false;
-  });
-
-  $("table").tablesorter({
-    widthFixed: true,
-    sortList: [[3, 1]]
-  }).tablesorterPager({
-    container: $("#pager")
   });
 });
