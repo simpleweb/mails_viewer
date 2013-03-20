7c3092b600df6e7c3d3f359804935c69c85d582c
Devon Noonan <dnoonan@infotech.com>
2013-03-20 10:47:01 -0400

    Use the document object and a selector so the events are bound to dynamically created objects.

--- app/assets/javascripts/mails_viewer.js
+++ app/assets/javascripts/mails_viewer.js
@@ -3,6 +3,18 @@
 //= require_tree .
 
 $(function() {
+  $(document).on('click', '.mail a.preview', function() {
+    console.log(this);
+    $(this).colorbox({width:"80%", height:"80%", iframe:true});
+  });
+
+  $(document).on('click', '.mail a.raw', function(){
+    $.get($(this).attr('href'), function(data){
+      $('#raw div').text(data);
+    });
+    return false;
+  });
+
   $("table").dataTable({
     "aaSorting": [[3, "desc"]],
     "aoColumns": [
@@ -14,15 +26,4 @@ $(function() {
     ]
   });
 
-  $('.mail a.preview').on('click', function() {
-    console.log(this);
-    $(this).colorbox({width:"80%", height:"80%", iframe:true});
-  });
-
-  $('.mail a.raw').on('click', function(){
-    $.get($(this).attr('href'), function(data){
-      $('#raw div').text(data);
-    });
-    return false;
-  });
 });
